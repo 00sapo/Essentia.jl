04dc9a6b74857947b8db158f7fb41f9aa2fb4611
Federico Simonetta <federico.simonetta@unimi.it>
2021-06-11 13:26:20 +0200

    Updated essentia

--- deps/build.jl
+++ deps/build.jl
@@ -4,15 +4,19 @@ ENV["JULIA_CXX_RTTI"] = 1
 cur_dir = pwd()
 cd(joinpath(@__DIR__, ".."))
 
-# update essentia
-run(`git submodule update --init`)
+if !isdir("essentia")
+    run(`git submodule update --init`)
+end
 
 # checkout last python release (need to find a way for auto select it...)
 cd("essentia")
-run(`git checkout ed59cc48`)
+# commit 13 january 2021 (2.1-b6)
+run(`git reset --hard 554502a06a39ebbe0de1b4797e3456ba18a090b1`)
+# commit 2019 (2.1-b5)
+# run(`git reset --hard ed59cc48`)
 
-# overwrite configuration (need to use `--disable-yasm`
-cp("./build_config.sh", "essentia/packaging/")
+# overwrite configuration (need to use `--disable-yasm`)
+cp("../build_config.sh", "./packaging/build_config.sh"; force=true)
 
 # cleaning previous configurations (if rebuilding...)
 try
